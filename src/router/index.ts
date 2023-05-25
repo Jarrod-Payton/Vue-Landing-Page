ab640e00592a9ff6227936bf8dfc18e0438570da
Jarrod-Payton <jarrodlpayton@gmail.com>
2023-05-25 02:46:00 -0500

    Trying again

--- src/router/index.ts
+++ src/router/index.ts
@@ -17,12 +17,12 @@ const router = createRouter({
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: AboutView
+      component: () => import('../pages/AboutView.vue')
     },
     {
       path: '/links',
       name: 'links',
-      component: LinkTreeView
+      component: () => import('../pages/LinkTreeView.vue')
     }
   ]
 })
